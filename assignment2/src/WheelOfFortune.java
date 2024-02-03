cba48faf9e107325207a7f65f92f8091874da07a
Mekrou <mills.ryan.011@gmail.com>
2024-02-03 17:30:07 -0500

    implemented my own substring algorithm. It's probably a little rough, but well tested!

--- assignment2/src/WheelOfFortune.java
+++ assignment2/src/WheelOfFortune.java
@@ -15,4 +15,53 @@ class WheelOfFortune
         }
         return false;
     }
+
+    
+    public boolean wordContainsString(String A, String B)
+    {
+        byte[] aAsBytes = A.getBytes();
+        byte[] bAsBytes = B.getBytes();
+        boolean beganMatch = false;
+        short firstOccuranceIndex = -1;
+        short lettersFound = 0;
+
+        // Explicit edge cases
+        if (B.length() == 0) // empty String
+            return true;
+        else if (B.length() > A.length()) // substring bigger than target String.
+            return false;
+        else if (B.length() == 1) { // single letter String
+            for (byte b : aAsBytes) {
+                if (b == bAsBytes[0])
+                    return true;
+            }
+            return false;
+        }
+
+        for (short i = 0; i < A.length(); i++)
+        {
+            if (beganMatch) {
+                short k = (short) (firstOccuranceIndex + 1);
+                for (short j = 1; j < B.length() + 1; j++) {
+                    if (aAsBytes[k] == bAsBytes[j]) {
+                        lettersFound++;
+                        k++;
+                        if (B.length() == lettersFound)
+                            return true;
+                    }
+                    else {
+                        beganMatch = false;
+                        lettersFound = 0;
+                        break;
+                    }
+                }
+            }
+            if (aAsBytes[i] == bAsBytes[0]) {
+                beganMatch = true;
+                lettersFound++;
+                firstOccuranceIndex = i;
+            }
+        }
+        return false;
+    }
 }
