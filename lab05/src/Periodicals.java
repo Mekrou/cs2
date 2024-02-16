215264edf5066e51daa2f482e3188cea55daa959
Mekrou <mills.ryan.011@gmail.com>
2024-02-16 17:21:59 -0500

    Implemented Periodicals

--- lab05/src/Periodicals.java
+++ lab05/src/Periodicals.java
@@ -1,3 +1,23 @@
 public class Periodicals extends Item {
+    private short issueNum;
     
+    public short getIssueNum() {
+        return this.issueNum;
+    }
+
+    public void setIssueNum(short issueNum) {
+        this.issueNum = issueNum;
+    }
+
+    public Periodicals() { }
+
+    public Periodicals(String title, short issueNum) {
+        super(title);
+        this.issueNum = issueNum;
+    }
+
+    public String getListing() {
+        return "Periodical Title - " + this.getTitle() + 
+        "\nIssue # - " + this.getIssueNum();
+    }
 }
