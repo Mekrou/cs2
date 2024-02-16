66aa433e7da7ee7365227c95f0bb40389fe25344
Mekrou <mills.ryan.011@gmail.com>
2024-02-16 16:59:20 -0500

    Implemented Item abstract class

--- lab05/src/Item.java
+++ lab05/src/Item.java
@@ -1,3 +1,24 @@
 abstract class Item {
-    
+    private String title;
+
+    public String getTitle() {
+        return this.title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Item() {
+        this.title = "";
+    }
+
+    public Item(String title) {
+        this.title = title;
+    }
+
+    protected abstract String getListing();
+
+    public String toString() {
+        return this.title;
+    }
 }
