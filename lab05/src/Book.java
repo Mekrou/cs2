045017f79152c2a840e39d985e6eb608df1be29f
Mekrou <mills.ryan.011@gmail.com>
2024-02-16 17:10:18 -0500

    Implemented Book class

--- lab05/src/Book.java
+++ lab05/src/Book.java
@@ -1,5 +1,35 @@
 public class Book extends Item {
-    protected void explain() {
-        System.out.println("I am a book");
+    private long isbn_number;
+    private String author;
+
+    public long getIsbn() {
+        return this.isbn_number;
+    }
+
+    public void setIsbn(long isbn) {
+        this.isbn_number = isbn;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Book() { }
+
+    public Book(String title, long isbn_number, String author) {
+        super(title);
+        this.isbn_number = isbn_number;
+        this.author = author;
+    }
+
+    protected String getListing() {
+        return 
+        "Book Name - " + getTitle() + 
+        "\nAuthor - " + getAuthor() + 
+        "\nISBN # - " + getIsbn();
     }
 }
