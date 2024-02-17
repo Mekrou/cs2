96b4b22582192cd076c1d5ea62dcad1f84fbd43c
Mekrou <mills.ryan.011@gmail.com>
2024-02-16 19:14:24 -0500

    Caterpie & Charmander

---
+++ assignment3/src/Caterpie.java
@@ -0,0 +1,5 @@
+public class Caterpie extends Pokemon {
+    public Caterpie(short level) {
+        super(level, 0.5, "Caterpie");
+    }
+}
---
+++ assignment3/src/Charmander.java
@@ -0,0 +1,5 @@
+public class Charmander extends Pokemon {
+    public Charmander(short level) {
+        super(level, 0.2, "Charmander");
+    }
+}
