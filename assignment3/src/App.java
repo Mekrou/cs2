e5a55adb9c921ca967593c9f1bf636a19426bca7
Mekrou <mills.ryan.011@gmail.com>
2024-02-17 14:56:06 -0500

    Implemented spawn() function

--- assignment3/src/App.java
+++ assignment3/src/App.java
@@ -3,11 +3,11 @@ import java.util.Random;
 public class App {
     public static void main(String[] args) throws Exception {
         for (int i = 0; i < 1000; i++) {
-            testSpawn();
+            spawn();
         }
     }
 
-    public static int spawn() {
+    public static Pokemon spawn() {
         Random random = new Random();
         final int level = random.nextInt(21);
         final int pokemonSwitch = random.nextInt(3) + 1;
@@ -17,27 +17,17 @@ public class App {
                 pokemon = new Bulbasaur(level);
                 break;
             case 2:
-            
+                pokemon = new Charmander(level);
                 break;
             case 3:
-            
+                pokemon = new Caterpie(level);
                 break;
             default:
+            pokemon = new Caterpie(level);
                 break;
         }
 
-        System.out.println("You encounter a level " pokemon.getLevel() + " " pokemon.getName());
-        return pokemonSwitch;
-    }
-
-    public static void testSpawn() {
-        int number = spawn();
-        System.out.println(number);
-        if (number > 3) {
-            throw new Error("Over 20");
-        }
-        else if (number < 1) {
-            throw new Error("Under 0");
-        }
+        System.out.println("You encounter a level " + pokemon.getLevel() + " " + pokemon.getName());
+        return pokemon;
     }
 }
