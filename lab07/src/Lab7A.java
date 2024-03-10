bf0d30d4e8a69d644348fee09861cc0efe619ed9
Mekrou <mills.ryan.011@gmail.com>
2024-03-10 19:15:02 -0400

    implemented recursive_mod

--- lab07/src/Lab7A.java
+++ lab07/src/Lab7A.java
@@ -17,7 +17,15 @@ public class Lab7A {
         return 1 + recursive_div(a - b, b);
     }
 
+    public static int recursive_mod(int a, int b) {
+        if (b == 0) return -1;
+        if (a == b) return 1;
+        if (a < b) return a;
+
+        return recursive_mod(a - b, b);
+    }
+
     public static void main(String[] args) {
-        System.out.println(recursive_div(7,2));
+        System.out.println(recursive_mod(10,0));
     }
 }
