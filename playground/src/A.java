320587b2254f085200e5e6ac75bbaa90c4d35f23
Mekrou <mills.ryan.011@gmail.com>
2024-03-10 22:35:08 -0400

    implemented isReverse()

--- playground/src/A.java
+++ playground/src/A.java
@@ -1,15 +1,27 @@
 public class A {
 
-  public static String question(String x) {
-    if (x.length() == 0) {
-      return x;
-    } else {
-      return "X" + question(x.substring(0, x.length() - 1));
-    }
-  }
+  
 
-  public static void main(String[] args) {
-    String y = question("ABCDE");
-    System.out.println(y);
+  
+public static void check_age(int age) throws Exception {
+  if(age<0) {
+    throw new Exception("Age cannot be below 0");
   }
+  else if(age>140) {
+    throw new Exception("That seems too high.");
+  }
+}
+
+public static void main(String[] args) {
+  
+try {
+  check_age(144);
+}
+catch(Exception e) {
+  System.out.println(e.getMessage());
+}
+
+}
+
+
 }
