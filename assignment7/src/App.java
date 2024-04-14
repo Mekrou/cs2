4efb30a32673b383a8d6515be1eab072e0d004b4
Mekrou <mills.ryan.011@gmail.com>
2024-04-14 09:57:54 -0400

    findFactor complete

--- assignment7/src/App.java
+++ assignment7/src/App.java
@@ -2,9 +2,20 @@ import java.util.LinkedList;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        final int max = 10000;
+
         Prime prime = new Prime(100);
-        prime.printPrimes();
-        System.out.println(prime.isPrime(6));
-        System.out.println(prime.getPrime(2));
+        int result = findFactor(10, prime);
+        System.out.println(result);
+    }
+
+    public static int findFactor(int target, Prime prime) {
+        for (int i = 0; i <= target; i++) {
+            int currPrime = prime.getPrime(i);
+            if ((target % currPrime) == 0) {
+                return currPrime;
+            }
+        }
+        return -1;
     }
 }
